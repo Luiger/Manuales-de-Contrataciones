8648e1054924aa1df3b4eb3494420496f16fe146
Luiger <luiger852@gmail.com>
2025-07-03 16:55:52 -0400

    qawer

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,7 @@ module.exports = {
   // Asegúrate de que las rutas en 'content' sean correctas para tu proyecto
   content: [
     "./App.{js,jsx,ts,tsx}",
-    "./screens/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
   
   // ---> AÑADE ESTA LÍNEA <---
