1072d172d34e2d8904b13bfa88e85c566d92f272
Luiger <luiger852@gmail.com>
2025-07-03 14:35:26 -0400

    añadiendo

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,29 +1,23 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const HomeScreen = () => {
   return (
-    <View className="flex-1 items-center justify-center bg-gray-100">
-      <TouchableOpacity className="bg-white p-4 rounded-lg shadow-md items-center m-2">
-        <View className="w-48 h-32 bg-gray-300 items-center justify-center">
-          <Text className="text-lg">Elaboración de Manual</Text>
-        </View>
-        <Text className="text-lg mt-2">Elaboración de Manual</Text>
-      </TouchableOpacity>
-      <TouchableOpacity className="bg-white p-4 rounded-lg shadow-md items-center m-2">
-        <View className="w-48 h-32 bg-gray-300 items-center justify-center">
-          <Text className="text-lg">Adquirir versión Pro</Text>
-        </View>
-        <Text className="text-lg mt-2">Adquirir versión Pro</Text>
-      </TouchableOpacity>
-      <TouchableOpacity className="bg-white p-4 rounded-lg shadow-md items-center m-2">
-        <View className="w-48 h-32 bg-gray-300 items-center justify-center">
-          <Text className="text-lg">Repositorio Legal</Text>
-        </View>
-        <Text className="text-lg mt-2">Repositorio Legal</Text>
-      </TouchableOpacity>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hola Mundo</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+  },
+});
 
 export default HomeScreen;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
+// tailwind.config.js
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
+  // Asegúrate de que las rutas en 'content' sean correctas para tu proyecto
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./screens/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  
+  // ---> AÑADE ESTA LÍNEA <---
+  presets: [require("nativewind/preset")],
+  
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
